eaaaf748f4bb56c9eea8235f3310301e94e19226
Danny Tatom <dannytatom@gmail.com>
2016-09-21 11:09:42 -0700

    Get project setup to run tests

---
+++ core/test/me/dannytatom/xibalba/systems/actions/MeleeSystemTest.java
@@ -0,0 +1,12 @@
+package me.dannytatom.xibalba.systems.actions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MeleeSystemTest {
+  @Test
+  public void shouldRemoveMeleeComponent() {
+    assertTrue(true);
+  }
+}
