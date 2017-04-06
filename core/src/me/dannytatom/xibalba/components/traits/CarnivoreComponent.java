975bccf036ed39df94f37912c8f143dc89420b56
Danny Tatom <dannytatom@gmail.com>
2017-04-06 14:49:15 -0700

    Carnivore is a minor trait

--- core/src/me/dannytatom/xibalba/components/traits/CarnivoreComponent.java
+++ core/src/me/dannytatom/xibalba/components/traits/CarnivoreComponent.java
@@ -3,7 +3,7 @@ package me.dannytatom.xibalba.components.traits;
 import com.badlogic.ashley.core.Component;
 
 public class CarnivoreComponent implements Component {
-  public static final int cost = 2;
+  public static final int cost = 1;
   public static final String name = "Carnivore";
   public static final String description
       = "Eating raw corpses gives a chance to heal";
